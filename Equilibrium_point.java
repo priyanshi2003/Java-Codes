3c5f25c154c194ca2fca237bcde5e97c482df39a
Priyanshi Nagpal <74672192+priyanshi2003@users.noreply.github.com>
2022-12-20 21:52:33 +0530

    Add files via upload

---
+++ Equilibrium_point.java
@@ -0,0 +1,44 @@
+import java.util.*;
+public class Equilibrium_point {
+
+    int equilibrium(int arr[], int n)
+        {
+            int i, j;
+            int leftsum, rightsum;
+
+
+            for (i = 0; i < n; ++i) {
+
+                leftsum = 0;
+                for (j = 0; j < i; j++)
+                    leftsum += arr[j];
+
+
+                rightsum = 0;
+                for (j = i + 1; j < n; j++)
+                    rightsum += arr[j];
+
+
+                if (leftsum == rightsum)
+                    return i;
+            }
+
+
+            return -1;
+        }
+
+
+        public static void main(String[] args)
+        {
+            Equilibrium_point equi = new Equilibrium_point();
+            int arr[] = { -7, 1, 5, 2, -4, 3, 0 };
+            int arr_size = arr.length;
+
+
+            System.out.println(equi.equilibrium(arr, arr_size));
+        }
+    }
+
+
+
+
