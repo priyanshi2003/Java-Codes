55c6bebf156e3ff1ad80f4e2800b2276f11500ae
Priyanshi Nagpal <74672192+priyanshi2003@users.noreply.github.com>
2022-12-25 22:12:59 +0530

    Add files via upload

---
+++ majority_element.java
@@ -0,0 +1,42 @@
+import java.io.*;
+public class majority_element {
+    static void findMajority(int arr[], int n)
+        {
+            int maxCount = 0;
+            int index = -1; // sentinels
+            for (int i = 0; i < n; i++) {
+                int count = 0;
+                for (int j = 0; j < n; j++) {
+                    if (arr[i] == arr[j])
+                        count++;
+                }
+
+
+                if (count > maxCount) {
+                    maxCount = count;
+                    index = i;
+                }
+            }
+
+
+            if (maxCount > n / 2)
+                System.out.println(arr[index]);
+
+            else
+                System.out.println("No Majority Element");
+        }
+
+
+        public static void main(String[] args)
+        {
+
+            int arr[] = { 1, 1, 2, 1, 3, 5, 1 };
+            int n = arr.length;
+
+
+            findMajority(arr, n);
+        }
+
+    }
+
+
